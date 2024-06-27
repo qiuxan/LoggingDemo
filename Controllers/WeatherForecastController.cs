136c17bac07b19daa97e84df83a7795523c502a2
qiuxan <qiuxan@gmail.com>
2024-06-27 20:07:48 +1000

    Log everytime get request

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.Log(LogLevel.Information, "This is a logging message.");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
